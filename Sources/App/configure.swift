427bdf03f90fd9793576a93a0c01c7aa27eef4fd
sanengineer <48153104+sanengineer@users.noreply.github.com>
2022-03-09 23:31:40 +0700

    try migrate

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,28 +6,48 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    guard let databaseUrl = Environment.get("DATABASE_URL") else {
-        return print("No Env Server Hostname")
+   if let dbUrlEnv = Environment.get("DATABASE_URL_2"), var postgresConfig = PostgresConfiguration(url: dbUrlEnv) {
+        postgresConfig.tlsConfiguration = .makeClientConfiguration()
+        postgresConfig.tlsConfiguration?.certificateVerification = .none
+        app.databases.use(.postgres(
+            configuration: postgresConfig
+        ), as: .psql)
+    } else {
+    // if let dbUrlEnv = Environment.get("DATABASE_URL"){
+    //     dbUrl = dbUrlEnv
+    //     try app.databases.use(.postgres(url: dbUrl), as: .psql)
+    //     print("DB_URL: \(dbUrl)")
+    // } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DB_HOSTNAME")!,
+            port: Environment.get("DB_PORT").flatMap(Int.init(_:))!,
+            username: Environment.get("DB_USERNAME")!,
+            password: Environment.get("DB_PASSWORD")!,
+            database: Environment.get("DB_NAME")!),
+            as: .psql)
+
+        // app.databases.use(.postgres()
     }
 
-    var postgresConfig = PostgresConfiguration(url: databaseUrl) 
-    postgresConfig.tlsConfiguration = .makeClientConfiguration()
-    postgresConfig.tlsConfiguration?.certificateVerification = .none
+     let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+
+    // Only add this if you want to enable the default per-route logging
+    let routeLogging = RouteLoggingMiddleware(logLevel: .info)
+
+    // Add the default error middleware
+    let error = ErrorMiddleware.default(environment: app.environment)
+    // Clear any existing middleware.
+    app.middleware = .init()
+    app.middleware.use(cors)
+    app.middleware.use(routeLogging)
+    app.middleware.use(error)
     
-
-    app.databases.use(.postgres(
-        configuration: postgresConfig
-    ), as: .psql)
-
-
-    // app.databases.use(.postgres(
-    //     hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-    //     port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-    //     username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-    //     password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-    //     database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        
-    // ), as: .psql)
+    app.logger.logLevel = .debug
 
     app.migrations.add(CreateTodo())
 
